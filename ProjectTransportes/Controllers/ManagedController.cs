a5306ff0785fdbeaec64f0877defaf1ee0145d1c
michelyp <michelypintom@gmail.com>
2024-03-12 13:45:32 +0100

    added:panel admin

---
+++ ProjectTransportes/Controllers/ManagedController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectTransportes.Controllers
+{
+    public class ManagedController : Controller
+    {
+        public IActionResult PanelAdmin()
+        {
+            return View();
+        }
+    }
+}
